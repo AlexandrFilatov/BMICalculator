fc751279e47d792b459e9bfadd79bdcb07d355b8
GTA <Phill23alex@gmail.com>
2021-01-29 20:12:48 +0300

    Add calc

---
+++ src/test/java/BMICalculatorDZ.java
@@ -0,0 +1,71 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BMICalculatorDZ {
+
+    @Test
+    public void calculateKgCmNormal() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // 1. Open site https://healthunify.com/bmicalculator/
+        driver.get("https://healthunify.com/bmicalculator/");
+        // 2. Enter 80 in Weight field
+        driver.findElement(By.name("wg")).sendKeys("80");
+        // 3. Enter 180 in Height field
+        driver.findElement(By.name("ht")).sendKeys("180");
+        // 4. Click Calculate button
+        driver.findElement(By.name("cc")).click();
+        String actualCategoryText = driver.findElement(By.name("desc")).getAttribute("value");
+        String expectedCategoryText = "Your category is Normal";
+        // 5. Check text "Your category is Normal" is displayed
+        Assert.assertEquals(actualCategoryText,expectedCategoryText, "Your category is Normal");
+
+        driver.close();
+    }
+    @Test
+    public void calculateKgCmObese() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // 1. Open site https://healthunify.com/bmicalculator/
+        driver.get("https://healthunify.com/bmicalculator/");
+        // 2. Enter 140 in Weight field
+        driver.findElement(By.name("wg")).sendKeys("140");
+        // 3. Enter 180 in Height field
+        driver.findElement(By.name("ht")).sendKeys("180");
+        // 4. Click Calculate button
+        driver.findElement(By.name("cc")).click();
+        String actualCategoryText = driver.findElement(By.name("desc")).getAttribute("value");
+        String expectedCategoryText = "Your category is Obese";
+        // 5. Check text "Your category is Normal" is displayed
+        Assert.assertEquals(actualCategoryText,expectedCategoryText, "Incorrect" + actualCategoryText);
+
+        driver.close();
+    }
+
+    @Test
+    public void calculateUnderweight() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        // 1. Open site https://healthunify.com/bmicalculator/
+        driver.get("https://healthunify.com/bmicalculator/");
+        // 2. Enter 50 in Weight field
+        driver.findElement(By.name("wg")).sendKeys("50");
+        // 3. Enter 175 in Height field
+        driver.findElement(By.name("ht")).sendKeys("175");
+        // 4. Click Calculate button
+        driver.findElement(By.name("cc")).click();
+        String actualCategoryText = driver.findElement(By.name("desc")).getAttribute("value");
+        String expectedCategoryText = "Your category is Underweight";
+        // 5. Check text "Your category is Normal" is displayed
+        Assert.assertEquals(actualCategoryText,expectedCategoryText, "Incorrect: " + actualCategoryText);
+
+        driver.close();
+    }
+
+}
